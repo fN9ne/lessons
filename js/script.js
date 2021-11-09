afb6ecf2d99643677daa54fcc8c0eabf58909178
fN9ne <39432487+fN9ne@users.noreply.github.com>
2021-11-09 23:01:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,4 +2,19 @@ $(document).ready(function() {
 	$('.sidebar__button').on('click', function() {
 		$('.sidebar').toggleClass('active')
 	});
+	function headerButton() {
+		if ($(window).width() <= 992) {
+			$('.sidebar').removeClass('active');
+			$('.header__button').unbind('click');
+			$('.header__button').on('click', function() {
+				$(this).toggleClass('active');
+				$('.header-menu').toggleClass('active');
+			});
+		} else {
+			$('.header__button').unbind('click');
+			$('.header__button').removeClass('active');
+			$('.header-menu').removeClass('active');
+		}
+	}
+	$(window).on('load resize', headerButton);
 });
